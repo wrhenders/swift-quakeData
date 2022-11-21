2487abd7c2139463709c8d052f841b07bf93f9e9
wrhenders <wrhenders@gmail.com>
2022-11-21 15:21:18 -0600

    Added Quake detail view and preview data

---
+++ Earthquakes/Preview Content/Quake+Preview.swift
@@ -0,0 +1,17 @@
+//
+//  Quake+Preview.swift
+//  Earthquakes-iOS
+//
+//  Created by Ryan Henderson on 11/21/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import Foundation
+
+extension Quake {
+    static var preview: Quake {
+        var quake = Quake(magnitude: 0.34, place: "Shakey Acres", time: Date(timeIntervalSinceNow: -1000), code: "nc73649170", detail: URL(string: "https://example.com")!)
+        quake.location = QuakeLocation(latitude: 38.809_333_8, longitude: -122.796_836_9)
+        return quake
+    }
+}
---
+++ Earthquakes/Views/QuakeDetail.swift
@@ -0,0 +1,35 @@
+//
+//  QuakeDetail.swift
+//  Earthquakes-iOS
+//
+//  Created by Ryan Henderson on 11/21/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct QuakeDetail: View {
+    var quake: Quake
+    @EnvironmentObject private var quakesProvider: QuakesProvider
+    
+    var body: some View {
+        VStack{
+            QuakeMagnitude(quake: quake)
+            Text(quake.place)
+                .font(.title3)
+                .bold()
+            Text("\(quake.time.formatted())")
+                .foregroundStyle(Color.secondary)
+            if let location = quake.location {
+                Text("Latitude: \(location.latitude.formatted(.number.precision(.fractionLength(3))))")
+                Text("Longitude: \(location.longitude.formatted(.number.precision(.fractionLength(3))))")
+            }
+        }
+    }
+}
+
+struct QuakeDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        QuakeDetail(quake: Quake.preview)
+    }
+}
